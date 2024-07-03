649e7ada9fca378eb32683b5b13f9b2560dd19a0
Hyane Moussassa <hyane.moussassa1@gmail.com>
2024-07-03 15:41:01 +0100

    done with step 1

---
+++ src/main/java/com/esgglobal/StringCalculator.java
@@ -0,0 +1,15 @@
+package com.esgglobal;
+
+import java.util.Arrays;
+
+public class StringCalculator {
+    public int add(String numbers) {
+        if (numbers == null || numbers.length() == 0){
+            return 0;
+        }
+        return Arrays.asList(numbers.split(","))
+                .stream()
+                .map(Integer::valueOf)
+                .reduce(0, Integer::sum);
+    }
+}
---
+++ src/test/java/com/esgglobal/StringCalculatorTest.java
@@ -0,0 +1,38 @@
+package com.esgglobal;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+
+    @Test
+    void testThatEmptyStringReturnsZero(){
+        StringCalculator stringCalculator = new StringCalculator();
+        int count = stringCalculator.add("");
+        Assertions.assertEquals(0, count);
+    }
+
+    @Test
+    void testThatNullStringReturnsZero(){
+        StringCalculator stringCalculator = new StringCalculator();
+        int count = stringCalculator.add(null);
+        Assertions.assertEquals(0, count);
+    }
+
+    @Test
+    void testThatStringContainingOneReturnsOne(){
+        StringCalculator stringCalculator = new StringCalculator();
+        int count = stringCalculator.add("1");
+        Assertions.assertEquals(1, count);
+    }
+    @Test
+    void testThatStringContainingOneCommaTwoReturnsThree(){
+        StringCalculator stringCalculator = new StringCalculator();
+        int count = stringCalculator.add("1,2");
+        Assertions.assertEquals(3, count);
+    }
+
+
+
+}
